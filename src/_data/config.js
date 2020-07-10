976f109adafabeed0b37e38677361eefaf6efbe0
garrettbland <gmorganbland@gmail.com>
2020-07-10 12:35:03 -0500

    Added pages, config, navbar, ect

---
+++ src/_data/config.js
@@ -0,0 +1,33 @@
+/**
+ * Gets added to global config object. Access it with 'config.[key]'
+ */
+module.exports = function () {
+    var post_site = 'Post Site'
+    var post_url = 'https://hayspost.com'
+
+    return {
+        title: 'Eagle Experts',
+        post_site: post_site,
+        post_url: post_url,
+        links: [
+            {
+                title: 'Home',
+                icon: '🏡',
+                url: '/',
+                target: '_self',
+            },
+            {
+                title: 'Advertise with us',
+                icon: '📝',
+                url: '/advertise',
+                target: '_self',
+            },
+            {
+                title: post_site,
+                icon: '🗞',
+                url: post_url,
+                target: '_blank',
+            },
+        ],
+    }
+}
