8b68d671d49d4d65833036fc3bff5dc56e258ab4
garrettbland <gmorganbland@gmail.com>
2021-01-22 14:40:40 -0600

    added gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,19 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8TH3ENSEP7" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-8TH3ENSEP7');
+                            `,
+                        }}
+                    />
+                </Head>
                 <body className="bg-blue-50">
                     <Main />
                     <NextScript />
